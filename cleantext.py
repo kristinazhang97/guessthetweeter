9d69c890ab8500d13e79e2318d0754b340f9266e
Emily Yu <67073459+emilyyu22@users.noreply.github.com>
2022-04-26 15:07:26 +0200

    clean text

--- cleantext.py
+++ cleantext.py
@@ -1,5 +1,10 @@
 import json
 import os
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import string
 
 def get_users():
     path = "/Users/emilyyu/Desktop/Exercises/final_project_json"
@@ -8,6 +13,32 @@ def get_users():
         if file.endswith(".json"):
             tweeters.append(os.path.splitext(file)[0])
     return tweeters
+
+def clean_tweet_bag(tweet: str) -> list:
+    # split tweet by whitespace to get tokens
+    tweet_tokens = tweet.split()
+
+    # filter out links
+    # tweet_tokens = [token for token in tweet_tokens if (token[:3] != "http" and token[:2] != "\\u")]
+    tweet_tokens = [token for token in tweet_tokens if (token[:3] != "http")]
+
+    # filter out punctuation
+    # tweet_tokens = [token for token in tweet_tokens if token.isalpha()]
+    table = str.maketrans('', '', string.punctuation)
+    tweet_tokens = [word.translate(table) for word in tweet_tokens]
+    # normalize case
+    tweet_tokens = [token.lower() for token in tweet_tokens]
+    # filter out stop words
+    # create a set of stop words
+    stop_words = set(stopwords.words("english"))
+    # filter out
+    tweet_tokens = [token for token in tweet_tokens if not token in stop_words]
+    # stem the text
+    porter = PorterStemmer()
+    tweet_tokens = [porter.stem(word) for word in tweet_tokens]
+    
+    # also need to remove links, @'s, emojis, 
+    return tweet_tokens
 
 #keys: data (contain all the tweets), meta (tells other information)
 #dictionary[0] 1/30 tweets
@@ -23,6 +54,7 @@ def get_user_tweets(user):
             if 'data' in dictionary[i]:
                 for j in range(len(dictionary[i]['data'])):
                     tweet = dictionary[i]['data'][j]['text']
+                    clean_bag_tweet(tweet)
                     user_tweets.append(tweet)
     return user_tweets
 

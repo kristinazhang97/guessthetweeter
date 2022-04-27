fd613604fea86a2d8522ebec5a7053fb9748db79
Emily Yu <67073459+emilyyu22@users.noreply.github.com>
2022-04-27 22:19:08 +0200

    Clean words works

--- cleantext.py
+++ cleantext.py
@@ -4,6 +4,7 @@ import nltk
 import string
 from text_preprocessing import *
 
+#returns a list of the 25 tweeter users
 def get_users():
     path = "/Users/emilyyu/Desktop/Exercises/final_project_json"
     tweeters = []
@@ -12,21 +13,15 @@ def get_users():
             tweeters.append(os.path.splitext(file)[0])
     return tweeters
 
+#returns a list of strings of clean words of the tweet
 def clean_tweet(text):
-    remove_URL(text)
-    remove_non_ascii(text)
-    to_lowercase(text)
-    remove_punctuation(text)
-    replace_numbers(text)
-    remove_stopwords(text)
-    #preprocess(text)
-    return text
+    tweet = remove_URL(text)
+    tweet = replace_contractions(tweet)
+    words = nltk.word_tokenize(tweet)
+    words = normalize(words)
+    return words
 
-#keys: data (contain all the tweets), meta (tells other information)
-#dictionary[0] 1/30 tweets
-#dictionary[0]['data'][0] 0-99 100 tweets in a dictionary
-#dictionary[0]['data'] data from every dictionary
-#(dictionary[0]['data'][0]['text']) Tweet from every dictionary print(dictionary[0]['data'][0]['text'])
+#returns a list of the users tweets
 def get_user_tweets(user):
     user_tweets = []
     path = "/Users/emilyyu/Desktop/Exercises/final_project_json"
@@ -36,12 +31,11 @@ def get_user_tweets(user):
             if 'data' in dictionary[i]:
                 for j in range(len(dictionary[i]['data'])):
                     tweet = dictionary[i]['data'][j]['text']
-                    clean_tweet(tweet)
+                    tweet = clean_tweet(tweet)
                     user_tweets.append(tweet)
     return user_tweets
 
-#keys: users, value: list of the users tweets
-
+#returns a dictionary of keys: tweeter users, value: list of the users tweets
 def get_tweets(user_dictionary):
     tweeters = get_users()
     for user in tweeters:
@@ -51,9 +45,9 @@ def get_tweets(user_dictionary):
 
 def main():
     #get_users()
-    #print(get_user_tweets('@barackobama')) 
-    print('Tune in at 11:35 a.m. ET to watch the President deliver a commencement speech at the U.S. Coast Guard Academy: http://t.co/0CUMi3p5BU')
-    print(clean_tweet('Tune in at 11:35 a.m. ET to watch the President deliver a commencement speech at the U.S. Coast Guard Academy: http://t.co/0CUMi3p5BU'))  
+    print(get_user_tweets('@barackobama')) 
+    #print('Tune in at 11:35 a.m. ET to watch the President deliver a commencement speech at the U.S. Coast Guard Academy: http://t.co/0CUMi3p5BU')
+    #print(clean_tweet('Tune in at 11:35 a.m. ET to watch the President deliver a commencement speech at the U.S. Coast Guard Academy: http://t.co/0CUMi3p5BU'))  
     #user_dictionary = {}
     #get_tweets(user_dictionary)
     
@@ -61,3 +55,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#Understanding the JSON
+#keys: data (contain all the tweets), meta (tells other information)
+#dictionary[0] 1/30 tweets
+#dictionary[0]['data'][0] 0-99 100 tweets in a dictionary
+#dictionary[0]['data'] data from every dictionary
+#(dictionary[0]['data'][0]['text']) Tweet from every dictionary print(dictionary[0]['data'][0]['text'])
